2c0877f660126d87e931036326348de05f9edc3e
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-08-25 22:07:04 +0300

    Create 1.rb

---
+++ public/1.rb
@@ -0,0 +1,25 @@
+{
+  'name': '',
+  'kof_markup': '',
+  'purchase_price_₴': '',
+  'personal_data': {
+    'coverage_area': "",
+    'metal_thickness': ""
+  }
+  'weight': '',
+  'dimensions': {
+    'height': '',
+    'length': '',
+    'width': '',
+    'diam': '',
+  },
+  'tags': '',
+  'photo': '',
+  'logo': '',
+  'producer': '',
+  'links_drawing': '',
+  'shipping_dimensions': '',
+  'unit': '',
+  'location_warehouse': '',
+  'group_crm': '',
+}
